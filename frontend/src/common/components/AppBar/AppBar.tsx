1d4afc90fcad735fbf4e4a190c67d79d37b5c736
Ivan Horbynko <horbynko.i@gmail.com>
2023-04-10 12:53:57 +0300

    create Team Table

--- frontend/src/common/components/AppBar/AppBar.tsx
+++ frontend/src/common/components/AppBar/AppBar.tsx
@@ -1,17 +1,14 @@
 import {
   AppBar as AppBarMui,
   Box,
-  FormControlLabel,
-  FormGroup,
   IconButton,
   Menu,
   MenuItem,
-  Switch,
   Typography,
   Toolbar,
   Button,
 } from "@mui/material";
-import { ChangeEvent, MouseEvent, useState } from "react";
+import { MouseEvent, useState } from "react";
 import { AccountCircle } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
 
@@ -24,10 +21,6 @@ export const AppBar = () => {
   const [auth, setAuth] = useState(true);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
-
   const handleMenu = (event: MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
@@ -37,60 +30,49 @@ export const AppBar = () => {
   };
 
   return (
-    /* ToDo remove below*/
-    <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-          label={auth ? "Logout" : "Login"}
-        />
-      </FormGroup>
-      {/* ToDo remove above*/}
+    <AppBarMui position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Logo
+        </Typography>
 
-      <AppBarMui position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Logo
-          </Typography>
+        {navLinks.map(({ name, route }) => (
+          <Box component={NavLink} key={name} to={route} sx={{ textDecoration: "none" }}>
+            <Button sx={{ color: "#fff" }}>{name}</Button>
+          </Box>
+        ))}
 
-          {navLinks.map(({ name, route }) => (
-            <Box component={NavLink} key={name} to={route} sx={{ textDecoration: "none" }}>
-              <Button sx={{ color: "#fff" }}>{name}</Button>
-            </Box>
-          ))}
-
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit">
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}>
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBarMui>
-    </Box>
+        {auth && (
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit">
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}>
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBarMui>
   );
 };
