b1863ba62557bb0f453537a3acd4e23f9b76102f
Ivan Horbynko <horbynko.i@gmail.com>
2023-06-01 15:49:56 +0300

    update dto UpdateOrganizationsInput

--- backend/src/modules/organizations/dto/update-org.input.ts
+++ backend/src/modules/organizations/dto/update-org.input.ts
@@ -1,42 +1,10 @@
-import { Field, ID, InputType, Int, registerEnumType } from '@nestjs/graphql';
-import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
-import { OrganizationStatus } from '../org.enums';
+import { PartialType, Field, ID, InputType } from '@nestjs/graphql';
+import { CreateOrganizationsInput } from './create-org.input';
 
 @InputType()
-export class UpdateOrganizationsInput {
+export class UpdateOrganizationsInput extends PartialType(
+  CreateOrganizationsInput,
+) {
   @Field(() => ID)
   id: number;
-
-  @Field(() => String, { nullable: true })
-  @IsNotEmpty()
-  title?: string;
-
-  @Field(() => String, { nullable: true })
-  description?: string;
-
-  @Field(() => OrganizationStatus, { nullable: true })
-  @IsEnum(OrganizationStatus, {
-    message: `Type mast be one of: ${Object.values(OrganizationStatus)}`,
-  })
-  @IsOptional()
-  status?: OrganizationStatus;
-
-  @Field(() => String, { nullable: true })
-  image?: string;
-
-  @Field(() => String, { nullable: true })
-  url?: string;
-
-  @Field(() => ID, { nullable: true })
-  parentId?: number;
-
-  @Field(() => [ID], { nullable: true })
-  users?: number[];
-
-  @Field(() => ID, { nullable: true })
-  creatorId?: number;
 }
-
-registerEnumType(OrganizationStatus, {
-  name: 'OrganizationStatus',
-});
